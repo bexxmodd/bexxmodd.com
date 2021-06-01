59432ee30362c3bd4c3a26f6ba99368e889ca307
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-05-31 21:35:32 -0400

    added render

--- bexxmodd_dot_com/blog/views.py
+++ bexxmodd_dot_com/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request: str):
-    return HttpResponse('<h1>Blog Home</h1>')
+def home(request):
+    return render(request, 'blog/home.html')
 
-def about(request: str):
-    return HttpResponse('<h1>Blog About</h1>')
+def about(request):
+    return render(request, 'blog/about.html')
