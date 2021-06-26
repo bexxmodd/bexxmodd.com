90c74a4ca11e2948ad79bb853ac88b3ebfecc3de
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-06-26 18:29:58 -0400

    comment section and new post

--- bexxmodd_dot_com/blog/views.py
+++ bexxmodd_dot_com/blog/views.py
@@ -23,30 +23,6 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-    def post_detail(self, request, slug):
-        template_name = 'post_detail.html'
-        post = get_object_or_404(Post, slug=slug)
-        comments = post.comments.filter(active=True)
-        new_comment = None
-
-        # Comment posted
-        if request.method == 'POST':
-            comment_form = CommentForm(data=request.POST)
-            if comment_form.is_valid():
-                # Create Comment object but don't save to database yet
-                new_comment = comment_form.save(commit=False)
-                # Assign the current post to the comment
-                new_comment.post = post
-                # Save the comment to the database
-                new_comment.save()
-        else:
-            comment_form = CommentForm()
-
-        return render(request, template_name, {'post': post,
-                                                'comments': comments,
-                                                'new_comment': new_comment,
-                                                'comment_form': comment_form})
-
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
