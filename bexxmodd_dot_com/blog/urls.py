a30931e8fe5aafc9d426bdfa347919cf2df1a18e
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-08-10 09:32:27 -0400

    added slugs instead of pk for post and comment

--- bexxmodd_dot_com/blog/urls.py
+++ bexxmodd_dot_com/blog/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('blog/', PostListView.as_view(), name='blog-posts'),
     path('post/<slug>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/comment/',
+    path('post/<slug>/comment/',
          CommentCreateView.as_view(), name='comment-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
